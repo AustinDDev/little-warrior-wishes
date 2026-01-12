3fa697740d7cd9ef4a433a09021cdb14a1fe8dc3
AustinDDev <austin.dalton.dev@gmail.com>
2026-01-12 14:45:47 -0500

    Simplify contact page to restore default export and fix build

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,42 +1,33 @@
 "use client";
 
-const FORMSPREE_ENDPOINT = "https://formspree.io/f/xpqqaplr";
-
 export default function Page() {
   return (
     <main style={{ padding: 24 }}>
       <h1>Contact</h1>
 
-      <form action={FORMSPREE_ENDPOINT} method="POST">
-        <input type="hidden" name="_subject" value="New Contact Form Message" />
-
-        <div style={{ marginTop: 12 }}>
+      <form action="https://formspree.io/f/xpqqaplr" method="POST">
+        <p>
           <label>
-            Name
-            <br />
+            Name<br />
             <input name="name" required />
           </label>
-        </div>
+        </p>
 
-        <div style={{ marginTop: 12 }}>
+        <p>
           <label>
-            Email
-            <br />
+            Email<br />
             <input name="email" type="email" required />
           </label>
-        </div>
+        </p>
 
-        <div style={{ marginTop: 12 }}>
+        <p>
           <label>
-            Message
-            <br />
+            Message<br />
             <textarea name="message" rows={6} required />
           </label>
-        </div>
+        </p>
 
-        <button style={{ marginTop: 12 }} type="submit">
-          Send
-        </button>
+        <button type="submit">Send</button>
       </form>
     </main>
   );
